1e3276a161404afe15759383384e49f6e9898298
edityomurti <edit@extrainteger.com>
2019-01-29 01:51:16 +0700

    Scraping profil caleg dprd prov

--- get_profil_caleg_dpr_prov.py
+++ get_profil_caleg_dpr_prov.py
@@ -13,7 +13,7 @@ def getData(indexOf, totalData, idCaleg, idPartai, idDapil, idPro):
 
 
 	context = ssl._create_unverified_context()
-	url_link = urllib.request.urlopen("https://infopemilu.kpu.go.id/pileg2019/pencalonan/calon/{}}".format(idCaleg), context = context)
+	url_link = urllib.request.urlopen("https://infopemilu.kpu.go.id/pileg2019/pencalonan/calon/{}".format(idCaleg), context = context)
 
 	try:
 		data = url_link.read().decode("utf8")
@@ -22,25 +22,34 @@ def getData(indexOf, totalData, idCaleg, idPartai, idDapil, idPro):
 
 		soup = BeautifulSoup(data, "html.parser")
 
-		listData = soup.find_all("div", class_="row no-margin yellow-one")
+		listDataRaw = soup.find_all("div", class_=["row no-margin", "row no-margin yellow-one"])
+
+		listData = list()
+
+		for i,item in enumerate(listDataRaw):
+			if (i<18):
+				listData.append(item)
 
 		
-		content = [idCaleg, idPartai, idDapil, idPro, item.find("div", {'class' : 'col-sm-9'}).text for item in listData]
+		content = [idCaleg, idPartai, idDapil, idPro]
+		contentData = [item.find("div", {'class' : 'col-sm-9'}).text for item in listData]
+		content.extend(contentData)
 
-		if (index == 1) {
-			headers = ['id', 'idPartai', 'idDapil', 'idPro', header.find("div", {'class' : 'col-sm-3'}).text for header in listData]
+		if (indexOf == '1'):
+			headers = ['id', 'idPartai', 'idDapil', 'idPro']
+			headersData = [header.find("div", {'class' : 'col-sm-3'}).text for header in listData]
+			headers.extend(headersData)
 			f = open('data_profil_caleg_prov.csv', 'w')
 			writer = csv.writer(f)
 			writer.writerow(headers)
 			writer.writerow(content)
-		} else {
+		else:
 			f = open('data_profil_caleg_prov.csv', 'a')
 			writer = csv.writer(f)
-			writer.writerow(content)			
-		}
+			writer.writerow(content)
 
 		is_success = True
-		message_string = "–––––– ({}/{}) idProv = {} generated ––––––".format(indexOf, totalData, idCaleg)
+		message_string = "–––––– ({}/{}) idCaleg = {} generated ––––––".format(indexOf, totalData, idCaleg)
 	except Exception as e:
 		message_string = "ERROR !!! failed getting idCaleg = {} ({}/{}) - cause : {}".format(idCaleg, indexOf, totalData, e)
 		is_success = False
@@ -50,9 +59,9 @@ def getData(indexOf, totalData, idCaleg, idPartai, idDapil, idPro):
 
 
 try:
-    total_data_generated = 0
-    data_generated = 0
-    data_error = 0
+	total_data_generated = 0
+	data_generated = 0
+	data_error = 0
 
 	csv_file = 'data_caleg_prov.csv'
 	df = pd.read_csv(csv_file)
@@ -65,16 +74,16 @@ try:
 	message_string = "=== START GENERATING PROFIL CALEG DPRD PROV ==="
 
 	print(message_string)
-    pecker("", message_string)
+	pecker("", message_string)
 
-    for i,idCaleg in enumerate(id_list):
-    	if (getData(str(i+1), str(len(id_list)), str(idCaleg), idPartai_list[i], idDapil_list[i], idPro_list[i])):
-    		data_generated +1
-    	else:
-    		data_error += 1
+	for i,idCaleg in enumerate(id_list):
+		if (getData(str(i+1), str(len(id_list)), str(idCaleg), idPartai_list[i], idDapil_list[i], idPro_list[i])):
+			data_generated +1
+		else:
+			data_error += 1
 
-    total_data_generated = data_generated + data_error
-    message_string = "=== END GENERATING PROFIL CALEG DPRD PROV:: generated={}, error={}, total={} data generated ===".format(data_generated, data_error, total_data_generated)
+	total_data_generated = data_generated + data_error
+	message_string = "=== END GENERATING PROFIL CALEG DPRD PROV:: generated={}, error={}, total={} data generated ===".format(data_generated, data_error, total_data_generated)
 
 except Exception as e:
 	message_string = "ERROR !!! Failed generating profil caleg dprd prov –– {}".format(str(e))
